799f317e548f215308afe600d89c5492b71283a8
FUJISHIGE TEMMA <tenma.x0@gmail.com>
2023-01-28 21:51:27 +0900

    :gear: use poetry

--- od_mstar3/setup.py
+++ od_mstar3/setup.py
@@ -1,8 +1,9 @@
-from distutils.core import setup, Extension
+from distutils.core import Extension, setup
+
 from Cython.Build import cythonize
 
 setup(ext_modules = cythonize(Extension(
            "cpp_mstar",                                
            sources=["cython_od_mstar.pyx"], 
-           extra_compile_args=["-std=c++11"]
+           extra_compile_args=["-std=c++11", "-I/usr/local/include"]
       )))
